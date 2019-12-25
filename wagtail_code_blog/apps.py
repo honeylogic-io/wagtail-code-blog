5c0940a78c1ce51e52ecf9c9b25cd6147f90e891
Dani Hodovic <dani.hodovic@gmail.com>
2019-12-25 21:09:58 +0100

    Add gitlab CI

--- wagtail_code_blog/apps.py
+++ wagtail_code_blog/apps.py
@@ -2,11 +2,11 @@ from django.apps import AppConfig
 
 
 class WagtailBlogConfig(AppConfig):
-    name = 'wagtail_code_blog'
+    name = "wagtail_code_blog"
 
     def ready(self):
         try:
-            # pylint: disable=unused-import
+            # pylint: disable=unused-import,import-outside-toplevel
             import wagtail_code_blog.signals
         except ImportError:
             pass
