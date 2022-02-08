a07575423423bcefbcb7afa0df0aa31074212ecc
Dani Hodovic <dani.hodovic@gmail.com>
2022-02-08 22:29:43 +0200

    Use tailwind

--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,12 @@
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 
 urlpatterns = [
     path("cms/", include(wagtailadmin_urls)),
     path("", include(wagtail_urls)),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
