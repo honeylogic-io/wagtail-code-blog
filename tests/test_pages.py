26c93f25afb3793461f6b74c7458f37f75a0b848
adinhodovic <hodovicadin@gmail.com>
2022-09-26 21:30:41 +0200

    fix: Lint

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -9,65 +9,67 @@
 
 #  pytestmark = pytest.mark.django_db
 
+
 def test_passes():
     pass
+
 
 #####################################
 # https://github.com/harrislapiroff/wagtail-foliage/issues
 # Wagtail-foliage is unmaintained
 #####################################
 #  def test_blog_index(client):
-    #  pages = [
-        #  (
-            #  BlogIndexPage(title="index"),
-            #  [BlogPage(title="My post", body="some text", date=timezone.now())],
-        #  )
-    #  ]
-    #  with page_tree(pages):
-        #  [(index, [post])] = pages
-        #  res = client.get(index.get_url())
-        #  assert res.status_code == 200
-        #  assert list(res.context["posts"]) == [post]
+#  pages = [
+#  (
+#  BlogIndexPage(title="index"),
+#  [BlogPage(title="My post", body="some text", date=timezone.now())],
+#  )
+#  ]
+#  with page_tree(pages):
+#  [(index, [post])] = pages
+#  res = client.get(index.get_url())
+#  assert res.status_code == 200
+#  assert list(res.context["posts"]) == [post]
 
 
 #  def test_blog_page(client):
-    #  pages = [
-        #  (
-            #  BlogIndexPage(title="index"),
-            #  [
-                #  BlogPage(
-                    #  title="My post",
-                    #  date=timezone.now(),
-                    #  body="some text",
-                #  )
-            #  ],
-        #  )
-    #  ]
-    #  with page_tree(pages):
-        #  [(_, [post])] = pages
-        #  res = client.get(post.get_url())
-        #  assert res.status_code == 200
-        #  assert post.readtime.text == "1 min"
+#  pages = [
+#  (
+#  BlogIndexPage(title="index"),
+#  [
+#  BlogPage(
+#  title="My post",
+#  date=timezone.now(),
+#  body="some text",
+#  )
+#  ],
+#  )
+#  ]
+#  with page_tree(pages):
+#  [(_, [post])] = pages
+#  res = client.get(post.get_url())
+#  assert res.status_code == 200
+#  assert post.readtime.text == "1 min"
 
-        #  soup = BeautifulSoup(res.content, "html.parser")
-        #  ld_json = json.loads(soup.find("script").string)
-        #  assert ld_json["articleBody"] == "some text"
-        #  assert ld_json["headline"] == "My post"
-        #  assert ld_json["mainEntity"] == {"@id": "example.com", "@type": "WebPage"}
+#  soup = BeautifulSoup(res.content, "html.parser")
+#  ld_json = json.loads(soup.find("script").string)
+#  assert ld_json["articleBody"] == "some text"
+#  assert ld_json["headline"] == "My post"
+#  assert ld_json["mainEntity"] == {"@id": "example.com", "@type": "WebPage"}
 
 
 #  def test_canonical_url(client):
-    #  post = BlogPage(
-        #  title="My post",
-        #  canonical_url="https://foo.com",
-        #  body="some text",
-        #  date=timezone.now(),
-    #  )
-    #  pages = [(BlogIndexPage(title="index"), [post])]
-    #  with page_tree(pages):
-        #  [(_, [post])] = pages
-        #  res = client.get(post.get_url())
-        #  assert res.status_code == 200
-        #  soup = BeautifulSoup(res.content, "html.parser")
-        #  el = soup.find("link", attrs={"rel": "canonical"})
-        #  assert el["href"] == "https://foo.com"
+#  post = BlogPage(
+#  title="My post",
+#  canonical_url="https://foo.com",
+#  body="some text",
+#  date=timezone.now(),
+#  )
+#  pages = [(BlogIndexPage(title="index"), [post])]
+#  with page_tree(pages):
+#  [(_, [post])] = pages
+#  res = client.get(post.get_url())
+#  assert res.status_code == 200
+#  soup = BeautifulSoup(res.content, "html.parser")
+#  el = soup.find("link", attrs={"rel": "canonical"})
+#  assert el["href"] == "https://foo.com"
