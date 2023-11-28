3e5c8ea1d0a4a25486ae21601d5d2a00af769574
Adin Hodovic <hodovicadin@gmail.com>
2023-11-28 22:02:23 +0100

    feat: Use tailwind classes (#52)
    * feat: Use tailwind classes
    
    * fix: prose max length mobile
    
    * feat: Use daisy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 module.exports = {
-  content: [
-    './wagtail_code_blog/templates/**/*.html',
-  ],
+  content: ["./wagtail_code_blog/templates/**/*.html"],
   safelist: [],
   theme: {
     extend: {},
   },
   plugins: [
-      /**
-       * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
-       * for forms. If you don't like it or have own styling for forms,
-       * comment the line below to disable '@tailwindcss/forms'.
-       */
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/typography'),
-      require('@tailwindcss/line-clamp'),
-      require('@tailwindcss/aspect-ratio'),
+    /**
+     * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+     * for forms. If you don't like it or have own styling for forms,
+     * comment the line below to disable '@tailwindcss/forms'.
+     */
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+    require("daisyui"),
   ],
-}
+  daisyui: {
+    darkTheme: false,
+  },
+};
